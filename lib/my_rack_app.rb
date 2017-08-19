f9845df3c2c9a76f69d182b14cb1c3970ef0f13e
Linh Nguyen My <lnguyenmy1@gmail.com>
2017-08-19 22:45:01 +0100

    Refactored code to case statement

--- lib/my_rack_app.rb
+++ lib/my_rack_app.rb
@@ -1,20 +1,22 @@
+require "erb"
+
 class MyRackApp
   def call(env)
     case env["PATH_INFO"]
     when '/'
-      response('200', 'Welcome')
+      response('200', {page_title: "Welcome Page", header: "Welcome", content: "button"})
     when '/login'
-      response('200', "Login")
+      response('200', {page_title: "Login Page", header: "Login", content: "login-form"})
     else
-      response('404', 'Not found')
+      response('404', {page_title: "Error Page", header: "Error", content: "error"})
     end
   end
 
-  def response(code, body)
+  def response(code, vars)
     [
       code,
       {'Content-Type' => 'text/html'},
-      [body]
+      [ERB.new(File.read(__dir__ + "/../views/layout.erb")).result_with_hash(vars)]
     ]
   end
 
