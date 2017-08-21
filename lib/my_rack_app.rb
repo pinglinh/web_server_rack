43795b5ef6f42a45137b5b7edc09d9f293850e78
Linh Nguyen My <lnguyenmy1@gmail.com>
2017-08-21 22:03:45 +0100

    Added login form, error page and 3rd argument to response method

--- lib/my_rack_app.rb
+++ lib/my_rack_app.rb
@@ -1,22 +1,56 @@
 require "erb"
+require "pp"
+require "pry"
 
 class MyRackApp
   def call(env)
+    # binding.pry
     case env["PATH_INFO"]
     when '/'
-      response('200', {page_title: "Welcome Page", header: "Welcome", content: "button"})
+      response(
+        '200',
+        {page_title: "Welcome Page",
+          header: "Welcome",
+          content: "button"})
     when '/login'
-      response('200', {page_title: "Login Page", header: "Login", content: "login-form"})
+      if env["REQUEST_METHOD"] == "GET"
+        response(
+          '200',
+          {page_title: "Login Page",
+            header: "Login",
+            content: <<-HTML})
+          <form action="/login" method="POST">
+          <label for="username">Username</label>
+          <input type="text" name="username" id="username">
+          <label for="password">Password</label>
+          <input type="password" name="password" id="password">
+          <input type="submit">
+          </form>
+          HTML
+      else
+        response(
+          '302',
+          {
+            page_title: "Dashboard",
+            header: "Dashboard",
+            content: "hello user"
+          },
+          {"Location" => "/dashboard"})
+      end
     else
-      response('404', {page_title: "Error Page", header: "Error", content: "error"})
+      response(
+        '404',
+        {page_title: "Error Page",
+          header: "Error",
+          content: "error"})
     end
   end
 
-  def response(code, vars)
+  def response(code, vars, headers = {})
     [
       code,
-      {'Content-Type' => 'text/html'},
-      [ERB.new(File.read(__dir__ + "/../views/layout.erb")).result_with_hash(vars)]
+      {'Content-Type' => 'text/html'}.merge(headers),
+      [ERB.new(File.read(__dir__ + "/../views/layout.erb")).result_with_haxsh(vars)]
     ]
   end
 
