2f4e9e305a7ee0c44ed823f10cf4dbcbdc5b8936
Linh Nguyen My <lnguyenmy1@gmail.com>
2017-08-22 14:07:34 +0100

    Added tests for login page

--- spec/my_rack_app_spec.rb
+++ spec/my_rack_app_spec.rb
@@ -1,5 +1,6 @@
 require 'rack/test'
 require 'my_rack_app'
+require 'pry'
 
 describe MyRackApp do
   include Rack::Test::Methods
@@ -8,23 +9,43 @@ describe MyRackApp do
     MyRackApp.new
   end
 
-  it "renders the homepage at /" do
-    get '/'
-    expect(last_response).to be_ok
+  xdescribe "/" do
+    it "renders the homepage at /" do
+      get '/'
+      expect(last_response).to be_ok
+    end
+
+    it "shows welcome message" do
+      get '/'
+      expect(last_response.body).to include("Welcome")
+    end
   end
 
-  it "shows welcome message" do
-    get '/'
-    expect(last_response.body).to include("Welcome")
+  describe "/login" do
+    xit "renders the login page at /login" do
+      get '/login'
+      expect(last_response.status).to eq(200)
+      expect(last_response.body).to include("Login")
+    end
+
+    context "when the user posts correct credentials" do
+      it "takes the user to the dashboard" do
+        post('/login', "username" => "hello@gmail.com", "password" => "hello1")
+        expect(last_response.status).to eq(302)
+        expect(last_response.location).to eq("/dashboard")
+        expect(last_response.body).to include("hello user")
+        # expect(last_response.headers).to eq("Dashboard")
+      end
+    end
+
+    context "when the user posts incorrect credentials" do
+      it "re-renders the form and lets the user know there was an error"
+
+    end
   end
 
-  it "renders the login page at /
-  login" do
-    get '/login'
-    expect(last_response.body).to include("Login")
-  end
 
-  it "return 404 when we go to any other page" do
+  xit "return 404 when we go to any other page" do
     get '/fjdl'
     expect(last_response.status).to eq(404)
   end
