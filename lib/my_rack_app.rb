378034dba453855d27c8019a195655eb4975f009
Linh Nguyen My <lnguyenmy1@gmail.com>
2017-08-27 13:08:04 +0100

    Took login path out into a LoginController class

--- lib/my_rack_app.rb
+++ lib/my_rack_app.rb
@@ -2,8 +2,36 @@ require "erb"
 require "pp"
 require "pry"
 
+class LoginController
+  def get
+    response(
+      '200',
+      {page_title: "Login Page",
+        header: "Login",
+        content: <<-HTML})
+      <form action="/login" method="POST">
+      <label for="username">Username</label>
+      <input type="text" name="username" id="username">
+      <label for="password">Password</label>
+      <input type="password" name="password" id="password">
+      <input type="submit">
+      </form>
+      HTML
+  end
+
+  def response(code, vars, headers = {})
+    [
+      code,
+      {'Content-Type' => 'text/html'}.merge(headers),
+      [ERB.new(File.read(__dir__ + "/../views/layout.erb")).result_with_hash(vars)]
+    ]
+  end
+end
+
+
 class MyRackApp
   def call(env)
+    login_controller = LoginController.new
     case env["PATH_INFO"]
     when '/'
       response(
@@ -13,19 +41,7 @@ class MyRackApp
           content: "button"})
     when '/login'
       if env["REQUEST_METHOD"] == "GET"
-        response(
-          '200',
-          {page_title: "Login Page",
-            header: "Login",
-            content: <<-HTML})
-          <form action="/login" method="POST">
-          <label for="username">Username</label>
-          <input type="text" name="username" id="username">
-          <label for="password">Password</label>
-          <input type="password" name="password" id="password">
-          <input type="submit">
-          </form>
-          HTML
+        login_controller.get
       else
         response(
           '302',
