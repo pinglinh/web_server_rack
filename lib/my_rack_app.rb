e5dbf2f893dc5bc304148047c4f3af3efbd15c4d
Linh Nguyen My <lnguyenmy1@gmail.com>
2017-09-01 14:34:55 +0100

    Added SignupController

--- lib/my_rack_app.rb
+++ lib/my_rack_app.rb
@@ -59,6 +59,18 @@ class LoginController < BaseController
   end
 end
 
+class SignupController < BaseController
+  def get
+    response(
+      '200',
+      {
+        page_title: "Signup",
+        header: "Signup",
+        content: "Sign up"
+      })
+  end
+end
+
 class DashboardController < BaseController
   def get
     response(
@@ -89,29 +101,16 @@ class MyRackApp
     dashboard_controller = DashboardController.new(responder)
     welcome_controller = WelcomeController.new(responder)
     error_controller = ErrorController.new(responder)
+    signup_controller = SignupController.new(responder)
 
     routes = {
       '/' => welcome_controller,
       '/login' => login_controller,
       '/dashboard' => dashboard_controller,
+      '/signup' => signup_controller
     }
 
     controller = routes.fetch(env["PATH_INFO"], error_controller)
     controller.public_send(env["REQUEST_METHOD"].downcase)
-
-    # case env["PATH_INFO"]
-    # when '/'
-    #   welcome_controller.get
-    # when '/login'
-    #   if env["REQUEST_METHOD"] == "GET"
-    #     login_controller.get
-    #   else
-    #     login_controller.post
-    #   end
-    # when '/dashboard'
-    #   dashboard_controller.get
-    # else
-    #   error_controller.get
-    # end
   end
 end
